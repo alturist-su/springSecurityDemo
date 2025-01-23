803831d94dffd35d052cdbf9440c1ed82ca47ca3
susingh <aimsushmitasingh@gmail.com>
2025-01-23 23:39:37 +0530

    Added csrf token

--- src/main/java/com/spring/security/springSecurity/controller/StudentController.java
+++ src/main/java/com/spring/security/springSecurity/controller/StudentController.java
@@ -1,28 +1,37 @@
 package com.spring.security.springSecurity.controller;
 
 import com.spring.security.springSecurity.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudentController {
 
     //define some students
-    public List<Student> students = List.of(
+    public List<Student> students = new ArrayList<>(List.of(
             new Student(1, "John", 90),
             new Student(2, "Jane", 85),
             new Student(3, "Tom", 78),
             new Student(4, "Lucy", 92)
-    );
+    ));
 
     //getting all students
     @GetMapping("/students")
     public List<Student> getAllStudents(){
         return students;
+    }
+
+    //get csrf token
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 
     //create a new student
